0cd985dd52c6b31d4063a3b00015984dcc586435
erika-tyagi <erika.tyagi@gmail.com>
2021-01-08 07:22:43 -0600

    Added response correlation analysis

--- vaccine-edu/vaccine-education-analysis.R
+++ vaccine-edu/vaccine-education-analysis.R
@@ -10,18 +10,28 @@ data <- data %>%
     filter(!is.na(`Accept?`)) %>% 
     mutate(Reason_clean = str_squish(Reason), 
            Reason_clean = str_remove_all(Reason_clean, "[^[:space:]|^[:alnum:]]"), 
-           Reason_clean = str_to_lower(Reason_clean))
+           Reason_clean = str_to_lower(Reason_clean), 
+           Reason_clean = str_remove(Reason_clean, "he "), 
+           Reason_clean = str_remove(Reason_clean, "she "), 
+           Reason_clean = str_remove(Reason_clean, "i "), 
+           Reason_clean = str_remove(Reason_clean, "am "), 
+           Reason_clean = str_remove(Reason_clean, "is "), 
+           Reason_clean = str_remove(Reason_clean, " it"), 
+           Reason_clean = str_replace(Reason_clean, "doesnt", "does not"), 
+           Reason_clean = str_replace(Reason_clean, "dont", "does not"), 
+           Reason_clean = str_replace(Reason_clean, "do not", "does not"), 
+           Reason_clean = str_replace(Reason_clean, "wants", "want"))
 
 # Sort by frequency 
-value_counts <- data %>% 
+value_counts <- data %>%
     filter(`Accept?` %in% c("m", "n")) %>%
     filter(!is.na(Reason_clean)) %>% 
     group_by(Reason_clean) %>%
     count() %>% 
     arrange(-n, Reason_clean) %>% 
-    rename("Responses" = n)
+    rename("Responses" = n) 
 
-# write.csv(value_counts, "responses-by-frequency.csv", row.names = FALSE)
+write.csv(value_counts, "responses-by-frequency_v2.csv", row.names = FALSE)
 
 # Get basic summary stats 
 data %>% 
@@ -68,4 +78,26 @@ viz3 <- data %>%
 
 ggsave("jp-by-age.png", viz3)
 
+# Response by previous response 
+viz4 <- data %>% 
+    mutate(accept_lag = paste("Previous respondent:", lag(`Accept?`)), 
+           accept_lag = factor(
+               accept_lag, 
+               levels = c(
+                   "Previous respondent: y", "Previous respondent: n", "Previous respondent: m"))) %>% 
+    filter(!is.na(lag(`Accept?`))) %>% 
+    group_by(accept_lag, `Accept?`) %>% 
+    summarise(n = n()) %>% 
+    mutate(pct = n / sum(n), 
+           label = paste0(scales::percent(pct, accuracy = 1), "\n", "n=", n)) %>% 
+    ggplot(aes(x = `Accept?`, y = pct, label = label)) + 
+    geom_text(nudge_y = 0.04) + 
+    scale_y_continuous(limits = c(0, 0.65)) + 
+    geom_bar(stat = "identity") + 
+    facet_wrap(~accept_lag) + 
+    theme_bw(base_size = 14) + 
+    coord_flip() + 
+    labs(title = "Responses by previous response", 
+         subtitle = "Responses tended to correlate to the previous response")
 
+ggsave("response-by-previous.png", viz4)
