67207f00956144a962b6c9f6be151856dde596d7
erika-tyagi <erika.tyagi@gmail.com>
2021-01-15 16:36:32 -0600

    Cleaned up file names

---
+++ R/pinckneyville_1.15.21.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(behindbarstools)
+
+scrape_il <- read_scrape_data(T, T, state = "Illinois")
+
+out <- plot_fac_trend(fac_name = "PINCKNEYVILLE CORRECTIONAL CENTER", 
+               state = "Illinois", 
+               metric = "Residents.Confirmed", 
+               plot_days = 60, 
+               scrape_df = scrape_il, 
+               area_plot = TRUE, 
+               annotate = TRUE, 
+               auto_label = TRUE) + 
+    labs(title = "PINCKNEYVILLE CORRECTIONAL CENTER (Perry County, IL)", 
+         caption = "+350 cases since 2021 \n Peak of 279 active cases on 1/11/21 \n Population of 1,592 as of 12/31/20 (from IDOC website)")
+
+ggsave("pinckneyville_1.15.21.png", out, width = 14, height = 10)
+ggsave("pinckneyville_1.15.21.svg", out, width = 14, height = 10)
--- R/social_media_1.11.21.R
+++ R/social_media_1.11.21.R
@@ -6,32 +6,32 @@ scrape_df <- read_scrape_data(T, T, state = "California")
 # Recent spikes - active cases 
 plt1 <- plot_recent_fac_increases(scrape_df = scrape_df,
                           plot_days = 30, 
-                          num_fac = 3, 
+                          num_fac = 4, 
                           annotate = TRUE, 
                           metric = "Residents.Active") + 
     labs(color = "Facility", 
          y = "Reported Active Cases", 
          title = "Facilities with Recent Spikes in Active COVID-19 Cases")
 
-ggsave("spikes_CA_active_1.11.20.svg", plt1, width = 16, height = 10)
-ggsave("spikes_CA_active_1.11.20.png", plt1, width = 16, height = 10)
+ggsave("spikes_CA_active_1.12.20.svg", plt1, width = 16, height = 10)
+ggsave("spikes_CA_active_1.12.20.png", plt1, width = 16, height = 10)
 
 plt2 <- plot_recent_fac_increases(scrape_df = scrape_df,
                                   plot_days = 30, 
-                                  num_fac = 3, 
+                                  num_fac = 5, 
                                   annotate = TRUE, 
                                   metric = "Residents.Confirmed") + 
     labs(color = "Facility", 
          y = "Reported Cumulative Cases", 
          title = "Facilities with Recent Spikes in Cumulative COVID-19 Cases")
 
-ggsave("spikes_CA_cumulative_1.11.20.svg", plt2, width = 16, height = 10)
-ggsave("spikes_CA_cumulative_1.11.20.png", plt2, width = 16, height = 10)
+ggsave("spikes_CA_cumulative_1.12.20.svg", plt2, width = 16, height = 10)
+ggsave("spikes_CA_cumulative_1.12.20.png", plt2, width = 16, height = 10)
 
 
 # CMC CA Men's Colony - active 
 plt3 <- scrape_df %>% 
-    filter(Name == "CMC CALIFORNIA MENS COLONY") %>% 
+    filter(Name == "CALIFORNIA MENS COLONY") %>% 
     filter(Date > "2020-12-25") %>% 
     mutate(last_value = if_else(Date == max(Date), as.character(Residents.Active), NA_character_)) %>%
     ggplot(aes(x = Date, y = Residents.Active, label = last_value)) + 
@@ -45,20 +45,26 @@ plt3 <- scrape_df %>%
          y = "Reported Active Cases") + 
     theme(axis.text.y = element_text(vjust = -0.6, hjust = 0, margin = margin(r = -45)))
          
-ggsave("cmc_active_1.11.20.svg", plt3, width = 14, height = 10)
-ggsave("cmc_active_1.11.20.png", plt3, width = 14, height = 10)
+ggsave("cmc_active_1.12.20.svg", plt3, width = 14, height = 10)
+ggsave("cmc_active_1.12.20.png", plt3, width = 14, height = 10)
 
 
 # CMC CA Men's Colony - cumulative 
 historical_CA <- read.csv("https://raw.githubusercontent.com/uclalawcovid19behindbars/historical-data/main/data/CA_adult_facility_covid_counts_historical.csv")
 
-plt4 <- historical_CA %>% 
-    filter(Name == "CMC CALIFORNIA MENS COLONY") %>% 
+cmc <- historical_CA %>% 
+    filter(Name == "CMC CALIFORNIA MENS COLONY") %>%
     mutate(Date = lubridate::ymd(Date)) %>% 
-    mutate(last_value = if_else(Date == max(Date) & Residents.Active == 933, as.character(Residents.Confirmed), NA_character_)) %>%
+    bind_rows(scrape_df %>% filter(Name == "CALIFORNIA MENS COLONY"))
+
+plt4 <- cmc %>% 
+    filter(!is.na(Residents.Confirmed)) %>% 
+    select(Date, Residents.Confirmed) %>% 
+    distinct() %>% 
+    mutate(last_value = if_else(Date == max(Date), as.character(Residents.Confirmed), NA_character_)) %>%
     ggplot(aes(x = Date, y = Residents.Confirmed, label = last_value)) + 
     geom_line(size = 2.0, color = "#D7790F") + 
-    geom_area(aes(x = Date, y = Residents.Confirmed), fill = "#D7790F", alpha = 0.5) +
+    geom_area(fill = "#D7790F", alpha = 0.5) +
     scale_x_date(date_labels = "%b %d", breaks = "2 month") +
     ggrepel::geom_text_repel(na.rm = T, show.legend = F, size = 6, nudge_x = 1, point.padding = 0.1) +
     theme_behindbars() + 
@@ -67,5 +73,5 @@ plt4 <- historical_CA %>%
          y = "Cumulative Cases") + 
     theme(axis.text.y = element_text(vjust = -0.6, hjust = 0, margin = margin(r = -45)))
 
-ggsave("cmc_cumulative_1.11.20.svg", plt4, width = 14, height = 10)
-ggsave("cmc_cumulative_1.11.20.png", plt4, width = 14, height = 10)
+ggsave("cmc_cumulative_1.12.20.svg", plt4, width = 14, height = 10)
+ggsave("cmc_cumulative_1.12.20.png", plt4, width = 14, height = 10)
