a9222baaf5bca4bf2dacdde582fc3d415ec06be4
erika-tyagi <erika.tyagi@gmail.com>
2021-02-17 16:40:12 -0600

    Adjusted annotation position from new data

--- R/bellamy_creek_2.17.21.R
+++ R/bellamy_creek_2.17.21.R
@@ -13,20 +13,20 @@ out <- plot_fac_trend("BELLAMY CREEK CORRECTIONAL FACILITY",
     labs(title = "Active COVID-19 Cases at Bellamy Creek Correctional Facility", 
          y = "Reported Active Cases Among Incarcerated People") + 
     annotate("text", 
-             x = as.Date("2021-01-30"), 
-             y = 220, 
-             label = "First case of the B.1.1.7 \nvariant detected on Feb 8", 
+             x = as.Date("2021-01-28"), 
+             y = 300, 
+             label = "First case of the B.1.1.7 \nvariant confirmed on Feb 8", 
              size = 6, 
              color = "#555526")
     
 ggsave("bellamy.png", out, width = 14, height = 10)
+ggsave("bellamy.svg", out, width = 14, height = 10)
 
 out2 <- scrape_mi %>% 
     filter(Name == "BELLAMY CREEK CORRECTIONAL FACILITY") %>% 
     mutate(Staff.Act.Est = diff_roll_sum(Staff.Confirmed, Date)) %>% 
     filter(!is.na(Date)) %>% 
     filter(Date > "2021-01-01") %>% 
-    mutate(Res.Act.Est = diff_roll_sum(Residents.Confirmed, Date)) %>% 
     ggplot(aes(x = Date)) + 
     geom_line(aes(y = Residents.Active), color = "#D7790F", size = 1.5) +
     geom_line(aes(y = Staff.Act.Est), color = "#4C6788", size = 1.5) +
