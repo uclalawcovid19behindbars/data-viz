d359e7fc8f6f5caa51401f649f7bdec1503fe228
erika-tyagi <erika.tyagi@gmail.com>
2021-03-02 08:35:12 -0600

    feat: update geom_ribbon to match style guide

--- ad-hoc/california-vaccine.R
+++ ad-hoc/california-vaccine.R
@@ -12,11 +12,13 @@ plt <- scrape_ca %>%
     ggplot() + 
     geom_line(aes(x = Date, y = pop), size = 2.0, color = "#2D6C4A") + 
     geom_line(aes(x = Date, y = res_vax), size = 2.0, color = "#D7790F") +
-    geom_area(aes(x = Date, y = res_vax), fill = "#D7790F", alpha = 0.5) + 
+    geom_area(aes(x = Date, y = res_vax), fill = "#D7790F", alpha = 0.3) + 
+    geom_ribbon(aes(x = Date, ymin = res_vax, ymax = pop), fill = "#82CAA4", alpha = 0.3) +
     theme_behindbars() + 
     scale_y_continuous(labels = scales::comma, limits = c(0, 100000)) + 
-    labs(title = "40% of incarcerated people in California's state prisons have been vaccinated", 
-         y = "Number of incarcerated people") + 
-    theme(axis.text.y = element_text(vjust = -0.6, hjust = 0.5, margin = margin(r = -45)))
+    labs(title = "44% of incarcerated people in California's state prisons have been vaccinated", 
+         y = "Number of incarcerated people", 
+         caption = "Vaccinated = 40,531 | Population = 91,310 | Percent Vaccinated = 44.4%")
 
 ggsave("ca_vaccine.png", plt, width = 16, height = 10)
+ggsave("ca_vaccine.svg", plt, width = 16, height = 10)
