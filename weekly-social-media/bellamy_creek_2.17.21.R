008ec5759d40420583a018918435d548ad17ffd7
erika-tyagi <erika.tyagi@gmail.com>
2021-03-30 12:51:13 -0500

    chore: update UK variant for op-ed

--- weekly-social-media/bellamy_creek_2.17.21.R
+++ weekly-social-media/bellamy_creek_2.17.21.R
@@ -6,21 +6,20 @@ scrape_mi <- read_scrape_data(T, T, state = "Michigan")
 out <- plot_fac_trend("BELLAMY CREEK CORRECTIONAL FACILITY", 
                state = "Michigan", 
                metric = "Residents.Active", scrape_df = scrape_mi, 
-               plot_days = 60, 
-               annotate = T, 
+               plot_days = 90, 
                area_plot = T) + 
     geom_vline(xintercept = as.Date("2021-02-08"), size = 1, color = "#555526") + 
     labs(title = "Active COVID-19 Cases at Bellamy Creek Correctional Facility", 
-         y = "Reported Active Cases Among Incarcerated People") + 
+         y = "Reported Active Cases Among Incarcerated People", 
+         caption = str_c("Data as of ", Sys.Date())) + 
     annotate("text", 
-             x = as.Date("2021-01-28"), 
-             y = 300, 
+             x = as.Date("2021-01-22"), 
+             y = 370, 
              label = "First case of the B.1.1.7 \nvariant confirmed on Feb 8", 
              size = 6, 
              color = "#555526")
     
 ggsave("bellamy.png", out, width = 14, height = 10)
-ggsave("bellamy.svg", out, width = 14, height = 10)
 
 out2 <- scrape_mi %>% 
     filter(Name == "BELLAMY CREEK CORRECTIONAL FACILITY") %>% 
@@ -33,6 +32,7 @@ out2 <- scrape_mi %>%
     theme_behindbars() + 
     labs(y = "Estimated Active Cases", 
          title = "Comparison of outbreaks beween staff and incarcerated people", 
-         subtitle = "Bellamy Creek Correctional Facility, Michigan")
+         subtitle = "Bellamy Creek Correctional Facility, Michigan", 
+         caption = str_c("Data as of ", Sys.Date()))
     
 ggsave("bellamy_staff_comparison.png", out2, width = 14, height = 10)
