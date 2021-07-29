260a91215f743bfa85863e45215322b98903c0b9
erika-tyagi <erika.tyagi@gmail.com>
2021-07-29 10:38:49 -0400

    update: add days late

---
+++ ad-hoc/days_last_update.R
@@ -0,0 +1,50 @@
+library(behindbarstools)
+library(tidyverse)
+
+last_df <- pull_last_update()
+
+plot_df <- last_df %>%
+    filter(!is.na(days_late)) %>%
+    # only get these kind of scrapers
+    filter(jurisdiction %in% c("state", "federal", "immigration")) %>%
+    # remove population scrapers here
+    filter(!stringr::str_detect(id, "(?i)population")) %>%
+    # remove youth scrapers here
+    filter(!stringr::str_detect(id, "(?i)youth")) %>%
+    # Do some renaming
+    mutate(State = ifelse(jurisdiction == "immigration", "ICE", State)) %>%
+    mutate(State = ifelse(jurisdiction == "federal", "BOP", State)) %>%
+    # average across all scrapers for a given agency
+    group_by(State) %>%
+    summarize(days_late = mean(days_late), .groups = "drop") %>%
+    mutate(State = forcats::fct_reorder(State, days_late))
+
+p <- plot_df %>% 
+    # Manually add this 
+    add_row(State = "GA", days_late = 12) %>% 
+    add_row(State = "MA", days_late = 13) %>% 
+    # Manually remove these 
+    filter(!State %in% c("AL", "DE")) %>% 
+    arrange(-days_late) %>% 
+    mutate(State_ = translate_state(State)) %>% 
+    filter(days_late >= 10) %>% 
+    mutate(State_ = forcats::fct_reorder(State_, days_late)) %>% 
+    ggplot(aes(x = State_, y = days_late, xend = State_, yend = 0, 
+               label = ceiling(days_late))) +
+    geom_point(size = 3, color = "#4C6788") +
+    geom_segment(size = 1.5, color = "#4C6788") +
+    geom_text(size = 5, position = position_nudge(y = 3.5), 
+              color = "#555526", family = "Helvetica") +
+    coord_flip() +
+    theme_behindbars(base_size = 18) +
+    theme(
+        panel.grid.major.y = element_blank(),
+        axis.text.y = element_text(color = "#555526"),
+        axis.text.x = element_text(color = "#555526"),
+        panel.grid.major.x = element_line(
+            color = "#92926C", linetype = "dotted"),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.position = "none") 
+
+ggsave("days_late.svg", p, width = 7, height = 4)
