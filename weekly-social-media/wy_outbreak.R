17aaff933c7c85950537bdc2e70d0bc9ec83ffc9
hjohns12 <hopecaneel@gmail.com>
2021-10-25 10:42:18 -0400

    chore: update WY social media plot

--- weekly-social-media/wy_outbreak.R
+++ weekly-social-media/wy_outbreak.R
@@ -15,6 +15,15 @@ out <- all_wy %>%
     scale_x_date(limits = c(as.Date("2021-08-01"), as.Date("2021-11-01"))) +
     theme_behindbars(base_size = 18, base_color = "black") + 
     labs(y = "Active COVID-19 cases") + 
-    scale_color_bbdiscrete()
+    scale_color_bbdiscrete() + 
+    theme(legend.position = "none", 
+          legend.title = element_blank())
 
 ggsave("wyoming_active.svg", out, width = 7, height = 5)
+
+
+## check latest numbers
+all_wy %>%
+    filter(Facility.ID %in% c(1716, 1715)) %>%
+    select(Date, Name, Residents.Active) %>%
+    View()
