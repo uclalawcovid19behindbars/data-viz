f45595377f5bb915d373f0f4d57054da94c17c84
hjohns12 <hopecaneel@gmail.com>
2021-10-25 09:13:06 -0400

    chore: update WY social media plot

--- weekly-social-media/wy_outbreak.R
+++ weekly-social-media/wy_outbreak.R
@@ -5,18 +5,16 @@ all_wy <- read_scrape_data(all_dates = T, state = "Wyoming")
 
 out <- all_wy %>% 
     ## filter to WYOMING MEDIUM CORRECTIONAL INSTITUTION
-    filter(Facility.ID == 1716) %>%
+    filter(Facility.ID %in% c(1716, 1715)) %>%
     filter(Date > "2021-08-01") %>% 
     group_by(Date) %>% 
-    # summarise(total_staff = sum_na_rm(Staff.Active), 
-    #           total_res = sum_na_rm(Residents.Active)) %>% 
-    ggplot(aes(x = Date)) + 
-    geom_line(aes(y = Residents.Active), color = "#D7790F", size = 1.0) + 
-    # geom_line(aes(y = Staff.Active), color = "#4C6788", size = 1.0) + 
+    ggplot(aes(x = Date, color = Name)) + 
+    # geom_line(aes(y = Residents.Active), color = "#D7790F", size = 1.0) + 
+    geom_line(aes(y = Residents.Active), size = 1.0) +
     scale_y_continuous(limits = c(0, 115)) +
     scale_x_date(limits = c(as.Date("2021-08-01"), as.Date("2021-11-01"))) +
-    theme_behindbars(base_size = 18) + 
-    # geom_vline(xintercept = as.Date("2021-06-13"), linetype = "dashed") + 
-    labs(y = "Active COVID-19 cases")
+    theme_behindbars(base_size = 18, base_color = "black") + 
+    labs(y = "Active COVID-19 cases") + 
+    scale_color_bbdiscrete()
 
 ggsave("wyoming_active.svg", out, width = 7, height = 5)
