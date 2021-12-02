9efc0c807af189c655637eff0c2ebe6387f80997
hjohns12 <hopecaneel@gmail.com>
2021-12-02 15:52:33 -0500

    chore: start code refactor

--- ad-hoc/year-to-year_change.R
+++ ad-hoc/year-to-year_change.R
@@ -1,9 +1,8 @@
 library(behindbarstools)
 library(tidyverse)
+library(zoo)
 
 ## LOAD DATA
-state_df <- "https://raw.githubusercontent.com/uclalawcovid19behindbars/data/master/historical-data/historical_state_counts.csv" %>% 
-    read_csv(col_types = cols()) 
 agg_week <- calc_aggregate_counts(state = T, week_grouping = TRUE, all_dates = T)
 covid_suffixes <- c(
     ".Confirmed", ".Deaths", ".Tadmin", ".Active", ".Initiated", ".Completed")
@@ -12,33 +11,49 @@ covid_suff <- paste(covid_suffixes, collapse = "|")
 ## TRANSFORM DATA
 agg_week_out <- agg_week %>%
     mutate(year = as.character(lubridate::year(Date)),
-           week = lubridate::week(Date)) %>% 
+           week = lubridate::week(Date),
+           month = lubridate::month(Date, label = T),
+           ) %>% 
     filter(stringr::str_detect(Measure, covid_suff)) %>%
     group_by(State, Measure) %>%
     arrange(Date) %>% 
     mutate(rollavg_measure = zoo::rollmean(Val, k = 3, fill = NA)) %>%
-    ungroup()
+    ungroup() %>%
+    arrange(week)
 
-## CREAT PLOTTING FUNCTION - works for a single metric
-plot_lags <- function(dat) {
+## 
+# State, metric, data, plot, perc_increase_since_sept
+# Alabama, res.confirmed, 
+create_plotting_vars <- function(dat) {
     transformed_dat <- dat %>%
         arrange(Date) %>%
         mutate(lag = Val - lag(Val)) %>%
         mutate(lag = ifelse(lag < 0, 0, lag),
-               dfr = diff_roll_sum(Val)) %>% ## might need to change the window on this to two, not sure
+               dfr = diff_roll_sum(Val, date_vec = Date)) %>% ## might need to change the window on this to two, not sure
         ## want diff_roll_sum for .Confirmed and lag for all else but .Active
         mutate(val_to_show = case_when(
             str_detect(metric, ".Confirmed") ~ dfr,
             str_detect(metric, ".Active")  ~ Val,
             TRUE ~ lag
         )) %>%
-        mutate(rollavg_weeklycase = zoo::rollmean(val_to_show, k = 3, fill = NA))
-    plot <- transformed_dat %>%
+        mutate(rollavg_weeklycase = zoo::rollmean(val_to_show, k = 3, fill = NA)) %>%
+        arrange(Date) %>%
+        mutate(lag_three_months = lag(Val, n = 12),
+               perc_ch_three_months = (Val - lag_three_months)/ lag_three_months)
+}
+
+## CREAT PLOTTING FUNCTION - works for a single metric
+plot_lags <- function(dat) {
+    plot <- dat %>%
         ggplot() + 
-        geom_line(aes(x = week, y = rollavg_weeklycase, color = year, ), stat = "identity") + 
+        geom_line(aes(x = week, y = rollavg_weeklycase, color = year), 
+                  stat = "identity") + 
         theme_behindbars(base_size = 14, base_color = "black") + 
         scale_y_continuous(label = scales::comma) +
-        theme(legend.position = "right", legend.title = element_blank()) 
+        scale_x_continuous(breaks = dat$week, labels = dat$month, n.breaks = 12) + 
+        # scale_x_date(date_breaks = "1 month", date_labels = "%b") + 
+        theme(legend.position = "right", legend.title = element_blank()) + 
+        labs(title = paste(unique(State), unique(Measure)))  
     return(plot)
 }
 
@@ -47,15 +62,29 @@ create_state_plots <- function(dat, metric) {
     dat %>%
         filter(Measure == metric) %>%
         split(.$State) %>%
+        .[c(1, 2, 3)] %>%
         imap(function(dat, State) {
             plot_lags(dat)
         }) 
 }
 
+## TESTING
+metric <- "Residents.Confirmed"
+testing_dat <- agg_week_out %>% 
+    filter(Measure == metric) %>%
+    split(.$State) %>%
+    .[c(1, 2, 3)]
+dat <- testing_dat[[1]]
+
 ## TO DO 
 # - add title
 # - add metric indicator 
 # - save plots somewhere 
+# - add flag for winter spike starting 2021
+# - add parameter to start_date, end_date
 
 agg_week_out %>%
     create_state_plots(metric = "Residents.Confirmed")
+
+agg_week_out %>%
+    create_state_plots(metric = "Staff.Confirmed")
