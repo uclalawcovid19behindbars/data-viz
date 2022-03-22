8791f70f412d35a2f9066ae26cd3f60d884bbedf
hjohns12 <hopecaneel@gmail.com>
2022-03-22 12:58:13 -0400

    update: add more to this after pair programming sesh w megan 3/22/22

--- ad-hoc/deaths_calc.R
+++ ad-hoc/deaths_calc.R
@@ -2,18 +2,55 @@ library(behindbarstools)
 library(tidyverse)
 Sys.Date() #[1] "2022-03-01"
 
+## TO DO - look into florida, april 11 2021 n_deaths = 430 then drops down to ~200
+
 ## read data
 root <- "https://raw.githubusercontent.com/uclalawcovid19behindbars/data/master/historical-data/"
 ntl_historical <- read_csv(str_c(root, "historical_national_counts.csv"))
 state_jur_historical <- read_csv(str_c(root, "historical_state_jurisdiction_counts.csv"))
 state_historical <- read_csv(str_c(root, "historical_state_counts.csv"))
 
+## FIRST LOOK AT THE ISSUE
+state_jur_death_sum <- state_jur_historical %>%
+    filter(Measure == "Residents.Deaths",
+           Web.Group == "Prison" ) %>%
+    group_by(Date, Web.Group) %>%
+    summarise(sum_deaths = sum_na_rm(Val))
+plot_ly(data = state_jur_death_sum, x = ~Date, y = ~sum_deaths, color = ~Web.Group)
+
 ## 1 - using stat jur: calc the number of new deaths nationally since summer 2021
 ## NB: noticed a big drop in deaths (~400) among state prisons between 6/20/21 and 6/27/21
 state_jur_deaths <- state_jur_historical %>%
     filter(Measure == "Residents.Deaths") %>%
-    group_by(Date, Web.Group) %>%
-    summarise(sum_deaths = sum_na_rm(Val))
+    select(-Rate) %>% 
+    group_by(State, Web.Group) %>%
+    mutate(prev_deaths = lag(Val, order_by = Date)) %>%
+    ungroup() %>%
+    mutate(change_deaths = Val - prev_deaths,
+           neg_change = change_deaths < 0,
+           change_gr_10 = abs(change_deaths) > 9,
+           neg_change_10 = neg_change & change_gr_10) #%>% 
+    # summarise(sum_deaths = sum_na_rm(Val))
+
+## Isolate to a certain date range: 
+## between June 20th and June 27th, 2021, the number of deaths among incarcerated  people
+
+states_dropped_june_21 <- state_jur_deaths %>%
+    # filter(neg_change_10) %>%
+    filter(neg_change) %>%
+    filter(Date > as.Date("2021-06-15"),
+           Date < as.Date("2021-07-01"))
+
+## was the web grouping changed in this date range?
+## this seems to have gotten "fixed" on september 5th the web grouping seems to have gotten fine again
+
+View(states_dropped_june_21)
+
+states_dropped_june_21 %>%
+    select(State) %>%
+    unique() %>%
+    pull(State)
+
 plot_ly(data = state_jur_deaths, x = ~Date, y = ~sum_deaths, color = ~Web.Group)
 
 ## 2- using nlt historical: calc the number of new deaths nationally since summer 2021
