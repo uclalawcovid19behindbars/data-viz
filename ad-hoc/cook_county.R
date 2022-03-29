5e6b2f7455b01e8cc8ddd30910b80e295a18e521
hjohns12 <hopecaneel@gmail.com>
2022-03-29 17:17:11 -0400

    update: get to the bottom of falling deaths count summer '21

--- ad-hoc/cook_county.R
+++ ad-hoc/cook_county.R
@@ -6,4 +6,10 @@ cook_county <- il_dat %>%
     filter(Facility.ID == 454)
 
 plot(cook_county$Date, cook_county$Residents.Confirmed)
+
+cook_county %>%
+    select(Date, Residents.Confirmed, Staff.Confirmed, Residents.Deaths, Staff.Deaths) %>%
+    arrange(Date) %>%
+    write_csv("~/Desktop/cook_county_covid.csv")
+   # View()
     
--- ad-hoc/deaths_calc.R
+++ ad-hoc/deaths_calc.R
@@ -1,5 +1,6 @@
 library(behindbarstools)
 library(tidyverse)
+library(plotly)
 Sys.Date() #[1] "2022-03-01"
 
 ## TO DO - look into florida, april 11 2021 n_deaths = 430 then drops down to ~200
@@ -18,59 +19,41 @@ state_jur_death_sum <- state_jur_historical %>%
     summarise(sum_deaths = sum_na_rm(Val))
 plot_ly(data = state_jur_death_sum, x = ~Date, y = ~sum_deaths, color = ~Web.Group)
 
-## 1 - using stat jur: calc the number of new deaths nationally since summer 2021
-## NB: noticed a big drop in deaths (~400) among state prisons between 6/20/21 and 6/27/21
-state_jur_deaths <- state_jur_historical %>%
-    filter(Measure == "Residents.Deaths") %>%
-    select(-Rate) %>% 
-    group_by(State, Web.Group) %>%
-    mutate(prev_deaths = lag(Val, order_by = Date)) %>%
-    ungroup() %>%
-    mutate(change_deaths = Val - prev_deaths,
-           neg_change = change_deaths < 0,
-           change_gr_10 = abs(change_deaths) > 9,
-           neg_change_10 = neg_change & change_gr_10) #%>% 
-    # summarise(sum_deaths = sum_na_rm(Val))
+## 0 - JUST LOOK AT THE THE TWO DATES THAT CHANGED
+## drop: between june 20 and june 27 2021
+## the states impacted are TX, IL, and AR
+state_jur_drop <- state_jur_historical %>%
+    filter(Measure == "Residents.Deaths",
+           Web.Group == "Prison",
+           Date %in% c(as.Date("2021-06-20"), as.Date("2021-06-27"))) %>%
+    select(-Rate) %>%
+    pivot_wider(names_from = Date, values_from = Val) %>%
+    mutate(alt_june_27 = ifelse(is.na(`2021-06-27`), 0, `2021-06-27`),
+            value_change = `2021-06-27` - `2021-06-20`,
+           value_change_with_na = alt_june_27 - `2021-06-20`,
+           big_change = value_change > 10)
 
-## Isolate to a certain date range: 
-## between June 20th and June 27th, 2021, the number of deaths among incarcerated  people
+## 1 - LOOK AT STATES THAT DROPPED in June '21
+# AR: no more data after 6/20/21
+# IL: data reporting gap between 6/20/21 and 8/15/21
+# TX: data reporting gap between 6/20/21 and 9/5/21
+states_dropped_hist <- state_jur_historical %>%
+    filter(Measure == "Residents.Deaths",
+           Web.Group == "Prison",
+           State %in% c("Texas", "Illinois", "Arkansas")) %>%
+    select(-Rate)
+plot_ly(data = states_dropped_hist, x = ~Date, y = ~Val, color = ~State)
 
-states_dropped_june_21 <- state_jur_deaths %>%
-    # filter(neg_change_10) %>%
-    filter(neg_change) %>%
-    filter(Date > as.Date("2021-06-15"),
-           Date < as.Date("2021-07-01"))
-
-## was the web grouping changed in this date range?
-## this seems to have gotten "fixed" on september 5th the web grouping seems to have gotten fine again
-
-View(states_dropped_june_21)
-
-states_dropped_june_21 %>%
-    select(State) %>%
-    unique() %>%
-    pull(State)
-
-plot_ly(data = state_jur_deaths, x = ~Date, y = ~sum_deaths, color = ~Web.Group)
-
-## 2- using nlt historical: calc the number of new deaths nationally since summer 2021
-deaths <- ntl_historical %>%
-    filter(Measure == "Residents.Deaths")
-plot_ly(data = deaths, x = ~Date, y = ~Count)
-deaths %>%
-    filter(Date == as.Date("2021-05-30") | 
-               Date == max(Date)) 
-
-## count 47 new deaths nationally, but that def can't be right nvm 
-
-#    Date       Measure             Count       Reporting      Missing                                                      
-# 2021-05-30  Residents.Deaths       2712        52 "Maine"                                                      
-# 2022-02-27  Residents.Deaths       2759        46 "Arkansas, Georgia, Louisiana, Maine, Massachusetts\n Missis…
-
-## 3 - calc the number of new deaths in BOP since summer 2021
-state_historical %>%
-    filter(State == "Federal") %>%
-    filter(Date == as.Date("2021-05-30") | 
-               Date == max(Date)) %>%
-    select(Date, Residents.Deaths)
-## count 38 new deaths since may 30 2021
+## 2 - LOOK AT STATES THAT GREW in September '21
+## TX: 100% of the jump comes from TX, where count rose from NA to 292
+## notable that the number of deaths in NJ fell from 105 to 52 (why?)
+state_jur_rise <- state_jur_historical %>%
+    filter(Measure == "Residents.Deaths",
+           Web.Group == "Prison",
+           Date %in% c(as.Date("2021-08-29"), as.Date("2021-09-05"))) %>%
+    select(-Rate) %>%
+    pivot_wider(names_from = Date, values_from = Val) %>%
+    mutate(alt_aug_count = ifelse(is.na(`2021-08-29`), 0, `2021-08-29`),
+           value_change = `2021-09-05` - `2021-08-29`,
+           value_change_with_na = `2021-09-05` - alt_aug_count,
+           big_change = value_change > 10)
